b1c965dc8a4501faef9b509449f34560e825297d
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2024-08-14 14:41:19 +0530

    making cart page responsive

--- client/src/pages/cart.js
+++ client/src/pages/cart.js
@@ -8,8 +8,9 @@ import CancelIcon from "@mui/icons-material/Cancel";
 
 function Cart() {
   const [cartItems, setCartItems] = useState([]);
-  const [subTotal,setSubTotal] = useState(0);
-  const [discount,setDiscount] = useState(25000);
+  const [subTotal, setSubTotal] = useState(0);
+  const [discount, setDiscount] = useState(18000);
+  const [checkOutFlag, setCheckOutFlag] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -20,10 +21,11 @@ function Cart() {
     dispatch(setCount(itemsObj.length));
 
     let totalPrice = 0;
-    cartItems.map((cartItem)=>totalPrice  += (cartItem.quantity*cartItem.price));
+    cartItems.map(
+      (cartItem) => (totalPrice += cartItem.quantity * cartItem.price)
+    );
     setSubTotal(totalPrice);
-
-  }, [cartItems,dispatch]);
+  }, [cartItems, dispatch]);
 
   useEffect(() => {
     let itemsObj = [];
@@ -45,7 +47,7 @@ function Cart() {
 
   const handleQuantity = (e, id) => {
     let value = e.target.value;
-    if(e.target.value == '') value = 0;
+    if (e.target.value == "") value = 0;
     let itemsObj = [];
     const items = localStorage.getItem("items");
     if (items == null) itemsObj = [];
@@ -66,69 +68,89 @@ function Cart() {
     }
   };
 
+  const handleBuyNow = () => {
+    setCheckOutFlag(true);
+    setTimeout(() => {
+      setCheckOutFlag(false);
+    }, 3000);
+  };
+
   return (
     <div className="relative">
       <Head>
         <title>Shop karo</title>
       </Head>
-      <div className="h-[90px] border-b"><Header /></div>
+      <div className="h-[90px] border-b">
+        <Header />
+      </div>
       <div>
+        {checkOutFlag && (
+          <div className="p-3 bg-green-200 sticky top-0 w-full z-[999] shadow-xl">
+            Successfully cart added
+          </div>
+        )}
         <div className="text-center font-medium p-10">Your Cart</div>
         <div className="mb-10">
-          {cartItems.length > 0 ? <table className="hidden lg:block w-max m-auto divide-y divide-gray-200 mb-10">
-            <thead className="bg-gray-100 bg-opacity-80">
-              <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Product
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Price
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Quantity
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Subtotal
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-gray-100 bg-opacity-60 shadow-sm">
-              {cartItems?.map((cartItem) => (
-                <tr key={cartItem.id}>
-                  <td className="px-3 py-4 whitespace-nowrap flex items-center justify-evenly w-[200px]">
-                    <CancelIcon
-                      sx={{ cursor: "pointer" }}
-                      onClick={() => handleOnDelete(cartItem.id)}
-                    />
-                    <Image
-                      src={cartItem.source}
-                      alt="image"
-                      unoptimized
-                      width={100}
-                      height={100}
-                    />
-                  </td>
-                  <td className="px-6 py-4 text-sm w-[350px]">
-                    {cartItem.title}
-                  </td>
-                  <td className="px-6 py-4">${cartItem.price}</td>
-                  <td className="px-6 py-4 w-[150px]">
-                    <input
-                      type="number"
-                      value={cartItem.quantity}
-                      min="1"
-                      className="border-2 outline-0 w-[100px]"
-                      onChange={(e) => handleQuantity(e, cartItem.id)}
-                    />
-                  </td>
-                  <td className="px-6 py-4">
-                    ${cartItem.price * cartItem.quantity}
-                  </td>
+          {cartItems.length > 0 ? (
+            <table className="hidden lg:block w-max m-auto divide-y divide-gray-200 mb-10">
+              <thead className="bg-gray-100 bg-opacity-80">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Product
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Price
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Quantity
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Subtotal
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table> : <div className="text-xl font-bold text-gray-400 text-center mb-10">Cart is empty</div>}
+              </thead>
+              <tbody className="bg-gray-100 bg-opacity-60 shadow-sm">
+                {cartItems?.map((cartItem) => (
+                  <tr key={cartItem.id}>
+                    <td className="px-3 py-4 whitespace-nowrap flex items-center justify-evenly w-[200px]">
+                      <CancelIcon
+                        sx={{ cursor: "pointer" }}
+                        onClick={() => handleOnDelete(cartItem.id)}
+                      />
+                      <Image
+                        src={cartItem.source}
+                        alt="image"
+                        unoptimized
+                        width={100}
+                        height={100}
+                      />
+                    </td>
+                    <td className="px-6 py-4 text-sm w-[350px]">
+                      {cartItem.title}
+                    </td>
+                    <td className="px-6 py-4">${cartItem.price}</td>
+                    <td className="px-6 py-4 w-[150px]">
+                      <input
+                        type="number"
+                        value={cartItem.quantity}
+                        min="1"
+                        className="border-2 outline-0 w-[100px]"
+                        onChange={(e) => handleQuantity(e, cartItem.id)}
+                      />
+                    </td>
+                    <td className="px-6 py-4">
+                      ${cartItem.price * cartItem.quantity}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div className="text-xl font-bold text-gray-400 text-center mb-10">
+              Cart is empty
+            </div>
+          )}
 
           <div className="space-y-5 pb-10 lg:hidden">
             {cartItems?.map((cartItem) => (
@@ -180,22 +202,30 @@ function Cart() {
             ))}
           </div>
 
-          <div className="w-[400px] sm:w-[600px] md:w-[750px] lg:w-[930px] m-auto">
-            <div className="border p-4 font-semibold">Cart totals</div>
-            <div className="border flex items-center justify-between p-4 text-[13px] md:text-[15px]">
-              <div className="font-medium">Subtotal:</div>
-              <div>${subTotal}</div>
+          {cartItems.length > 0 && (
+            <div className="w-[400px] sm:w-[600px] md:w-[750px] lg:w-[930px] m-auto">
+              <div className="border p-4 font-semibold">Cart totals</div>
+              <div className="border flex items-center justify-between p-4 text-[13px] md:text-[15px]">
+                <div className="font-medium">Subtotal:</div>
+                <div>${subTotal}</div>
+              </div>
+              <div className="border flex items-center justify-between p-4 text-[13px] md:text-[15px]">
+                <div className="font-medium">Discount:</div>
+                <div>${discount}</div>
+              </div>
+              <div className="border flex items-center justify-between p-4 text-[13px] md:text-[15px]">
+                <div className="font-medium">Total:</div>
+                <div>${subTotal - discount}</div>
+              </div>
+
+              <button
+                className="p-4 my-2 w-full bg-green-600 text-white font-medium text-[13px] md:text-[15px]"
+                onClick={handleBuyNow}
+              >
+                BUY NOW
+              </button>
             </div>
-            <div className="border flex items-center justify-between p-4 text-[13px] md:text-[15px]">
-              <div className="font-medium">Discount:</div>
-              <div>${discount}</div>
-            </div>
-            <div className="border flex items-center justify-between p-4 text-[13px] md:text-[15px]">
-              <div className="font-medium">Total:</div>
-              <div>${subTotal-discount}</div>
-            </div>
-            <button className="p-4 my-2 w-full bg-green-600 text-white font-medium text-[13px] md:text-[15px]">BUY NOW</button>
-          </div>
+          )}
         </div>
       </div>
     </div>
